05d4993006b23efe53eb758bc8d12386dbd84578
corineallany <corineallany@gmail.com>
2026-09-16 13:47:22 +0200

    Ajoute les anniversaires au pilotage

---
+++ pilotage-birthdays.js
@@ -0,0 +1,2 @@
+/* Anniversaires transversaux dans Pilotage. */
+const _pilotageWithBirthdays=pilotage;pilotage=function(){return _pilotageWithBirthdays()+`<div style="margin-top:16px">${birthdayCard()}</div>`}
